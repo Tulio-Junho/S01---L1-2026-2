63f99ccad204d1d7b76b458df07ad92f23003d0f
Tulio Junho <tulio.junho@ges.inatel.br>
2026-09-10 10:58:38 -0300

    Create Exercício 4.go

---
+++ Relatório 4/Exercício 4.go
@@ -0,0 +1,24 @@
+package main
+import "fmt"
+
+func ValidarCodigoRastreio(codigo string) (bool, string) {
+    if len(codigo) == 10 {
+        return true, "Codigo de rastreio registrado no sistema!"
+    } else {
+        return false, "Erro: O código de rastreio deve ter exatamente 10 caracteres."
+    }
+}
+
+func main() {
+    var status bool
+    var codigo string
+    var retorno string
+
+    for !status {
+        fmt.Printf("Digite o codigo de rastreio: ")
+        fmt.Scanln(&codigo)
+
+        status, retorno = ValidarCodigoRastreio(codigo)
+        fmt.Println(retorno)
+    }
+}
