de6d99bbd06fe33aae3fbaddea8d21e5bc92f1ed
Tulio Junho <tulio.junho@ges.inatel.br>
2026-09-10 11:14:05 -0300

    Rename Exercício 4.go

---
+++ Relatório 4/Exercício 4.go
@@ -0,0 +1,34 @@
+package main
+import "fmt"
+
+func validarIngresso(setor string, codigo int) (bool) {
+
+	if setor == "VIP" && codigo == 2026 {
+		return true
+
+	} else {
+
+		return false
+	}
+}
+
+func main() {
+
+	var setor string
+	var codigo int
+
+	for {
+		fmt.Print("Digite o setor do ingresso: ")
+		fmt.Scan(&setor)
+
+		fmt.Print("Digite o codigo do ingresso:\n ")
+		fmt.Scan(&codigo)
+
+		if validarIngresso(setor, codigo) {
+			fmt.Println("Acesso liberado para area VIP!")
+			break
+		} else {
+			fmt.Println("Ingresso ou setor invalido. Tente novamente.")
+		}
+	}
+}
